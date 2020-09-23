3929e7f5f7f0904ed8ee2059010413cfb88e1be7
Vincent He <vincent0710@Outlook.com>
2020-09-23 10:42:34 -0400

    Learning how to define complex type

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -6,6 +6,18 @@ Remember to take the practice tests for each topic.
 \
 Please [commit your work frequently](https://www.russellgordon.ca/cs/source-control/introduction/using-source-control.pdf) and document your progress once each day in [Spaces](http://ca.spacesedu.com).
  */
-import Cocoa
 
-var str = "Hello, playground"
+var truple = (first: "Vincent", last: "He") // Making a truple
+var array = [1,23,45,6,7,7] // Making an array with in swift
+var dict = [
+    "Jason" : ["Name" : "Jason Zhang", "Heigh" : 1.7],
+    "Vincent" : ["Name" :"Vincent He", "Heigh" : 2.5]
+]// This is an dict in swift
+
+enum Planet: Int {
+    case mercury
+    case venus
+    case earth
+    case mars
+}
+

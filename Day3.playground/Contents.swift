1673272129dec10030bb4abc9b5dc8b7f6c53dda
Vincent He <vincent0710@Outlook.com>
2020-09-24 09:47:09 -0400

    Learning conditionals such as || and &&

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -7,6 +7,23 @@ Remember to take the practice tests for each topic.
 Please [commit your work frequently](https://www.russellgordon.ca/cs/source-control/introduction/using-source-control.pdf) and document your progress once each day in [Spaces](http://ca.spacesedu.com).
  */
 
-import Cocoa
+import Cocoai
+import Fundamental
 
-var str = "Hello, playground"
+let a:Double = 30.0
+let b:Double = 10.0
+let state:Bool = true
+
+if (a+b) == 40.0 && a == 30{
+    print("hi")
+} // Uinsg the if statment to create true
+
+if state || a == 10{
+    print("True")
+} // Using the or statement
+
+for x in 3...10{
+    print(x)
+} // Printing from 3 to 10
+
+

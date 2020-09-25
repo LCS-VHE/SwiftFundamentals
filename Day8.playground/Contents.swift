68a999901432cc260499802056e638b202d5ca8e
Vincent He <vincent0710@Outlook.com>
2020-09-25 12:02:33 -0400

    Learning how to use struct in swift

--- Day8.playground/Contents.swift
+++ Day8.playground/Contents.swift
@@ -10,3 +10,45 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+// Making a structs
+struct TheBigMath{
+    var name:String
+    var value: Int{ // This is to be set to seom value before swift can detech soemthing changed
+        didSet{
+            print("Somethign has changed in the vector")
+        }
+    }
+    
+    // Define a method
+    func add(num1:Int, num2:Int) -> Int{
+        return num1+num2
+    }
+    
+    // Changing a variables that is in struc
+    mutating func change(){
+        name = "The name has changed"
+    }
+}
+
+var a = TheBigMath( name: "Cool Vector", value: 3)
+a.value = 23
+a.value = 13
+
+print(a.add(num1:1, num2:2)) // adding 1 and 2 together
+a.change() // Changing the name of the private varible within a struct
+print(a.name)
+
+let string = "This is a test string so that I could count how many string character there are"
+print(string.count) // A mehtod to count how many string there are
+print(string.lowercased()) // Converting string into lowercased
+print(string.uppercased()) // Converting string into uppercased
+print(string.sorted()) // Turn every character into an array
+
+var test = string.sorted()
+
+test.append(contentsOf: "Hello I am vincent") // adding element to list
+test.firstIndex(of: "I") // finding the index of something
+print(test)
+test.remove(at: 0)// remove an element that is in index 0
+

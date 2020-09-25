af14188e36c070a059c2225ac4fa742c756684fd
Vincent He <vincent0710@Outlook.com>
2020-09-25 11:27:02 -0400

    Learning the basic of functions in swift

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -8,5 +8,35 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
  */
 
 import Cocoa
+import Foundation
 
 var str = "Hello, playground"
+
+func lol() ->(){
+        print("HI")
+}
+
+func new_stuff(mes:String) -> (){
+    print(mes)
+}
+
+new_stuff(mes: "A New Stuff")
+lol()
+
+// Returning varibles with more than one value
+func names(n1: String, n2: String) -> (first:String, second:String) {
+    print("Name 1: \(n1), Name:2 \(n2)")
+    return (first: n1, second: n2)
+}
+
+print(names(n1: "Vincent", n2: "Jason").first, names(n1: "Vincent", n2: "Jason").second) // not a good way to do it calling function two times
+
+// Default parameters
+func new(n1: String="Vince", n2: String="Jason")  -> (){
+    print("HI, \(n1), \(n2)")
+}
+
+for x in 0...3{
+    new(n1: "Vince", n2 : "Jason")
+}
+

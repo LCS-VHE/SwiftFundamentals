540b58a15c8671d33baa8476c3fc4d53936c3e76
Vincent He <vincent0710@Outlook.com>
2020-09-25 12:20:29 -0400

    Finishing the class in swift day 10

--- Day10.playground/Contents.swift
+++ Day10.playground/Contents.swift
@@ -10,3 +10,54 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+// Making a class and struct are preety much the same
+class Student{
+    private var name:String
+    private var id:Int
+    
+    init(name:String, id:Int){
+        self.name = name
+        self.id = id
+    }
+    
+    func show() ->(){
+        print("Student Name: \(name), Id: \(id)")
+    }
+}
+let Vincent = Student(name: "Vincent", id: 12345)
+Vincent.show()
+
+// This is class inheritance in swift
+class Dog {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+    
+    func makenoise(){
+        print("HO HO HO")
+    }
+}
+
+class Poodle: Dog {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle") // define the parent class
+    }
+    
+    deinit{ // same is ~name() in c++
+        print("Dog is gone in memory forever")
+    }
+    
+    override func makenoise() { // Overriding function in swift
+        print("YO YO YO")
+    }
+}
+var a = Poodle(name: "The Big Man")
+print(a.breed)
+a.makenoise()
+
+

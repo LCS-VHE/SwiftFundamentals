74801c4017eec00f797e768d83d704fe8dbfbc4d
Vincent He <vincent0710@Outlook.com>
2020-10-06 21:33:12 -0400

    Update day 7 swift fundamental

--- Day7.playground/Contents.swift
+++ Day7.playground/Contents.swift
@@ -15,3 +15,21 @@ let a = { (mes: String, name: String) in
     print("\(mes), Said by \(name)")
 }
 a("Hi I am not really interseting","Vincent He")
+
+// Shorten parms name
+func travel(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+// Return closures from function
+func travel() -> (String) -> Void { // Returning a void closures
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+
+
